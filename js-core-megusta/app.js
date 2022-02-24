ac85d9b2ba8247e71ec2ab50774488422084d570
caceresrene <rene@outlook.my>
2022-02-23 21:06:23 -0300

    added js core megusta

---
+++ js-core-megusta/app.js
@@ -0,0 +1,22 @@
+var btnLike1 = document.getElementById('like-button1');
+var likes1 = document.getElementById('likes1');
+btnLike1.onclick = function () {
+	likes1.innerText = parseInt(likes1.innerText) + 1;
+};
+
+var btnLike2 = document.getElementById('like-button2');
+var likes2 = document.getElementById('likes2');
+btnLike2.onclick = function () {
+	likes2.innerText = parseInt(likes2.innerText) + 1;
+};
+
+var btnLike3 = document.getElementById('like-button3');
+var likes3 = document.getElementById('likes3');
+btnLike3.onclick = function () {
+	likes3.innerText = parseInt(likes3.innerText) + 1;
+};
+// var btnLike1 = document.getElementById('like-button2');
+// var likes1 = document.getElementById('likes1');
+// btnLike1.onclick = function () {
+// 	likes1.innerText = parseInt(likes1.innerText) + 1;
+// };
